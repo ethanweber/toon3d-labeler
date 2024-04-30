17f27478c0788dbd42968fd5079937735f9fc593
ethanweber <ethanweber@berkeley.edu>
2024-04-30 22:12:14 +0000

    Deploying to gh-pages from @ ethanweber/toon3d-labeler@93bbe3fdb9522a9659f54e53a31d799516062775 🚀

---
+++ precache-manifest.748733784fd65b0e56eae427a9c7ef80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f760feb9441694e12ec694761ac0a1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4b288ff87aef57181a0",
+    "url": "/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "dbcb1dd1e527f44d821a",
+    "url": "/static/css/main.25663984.chunk.css"
+  },
+  {
+    "revision": "f4b288ff87aef57181a0",
+    "url": "/static/js/2.69143c8d.chunk.js"
+  },
+  {
+    "revision": "e09e17a0976956a4dd59624114f0620f",
+    "url": "/static/js/2.69143c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e38035d58f4eb3452709",
+    "url": "/static/js/3.06a1c2d2.chunk.js"
+  },
+  {
+    "revision": "dbcb1dd1e527f44d821a",
+    "url": "/static/js/main.ad46bb20.chunk.js"
+  },
+  {
+    "revision": "f2f2f514083b7e6d0af4",
+    "url": "/static/js/runtime-main.7897c83b.js"
+  }
+]);
